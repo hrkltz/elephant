814da57eead73d03b9e1fe68ee201c79c607f19e
Oliver Herklotz <oliver@hrkltz.io>
2022-12-13 21:56:25 +0100

    switch from vite to webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,35 @@
-//Webpack requires this to work with directories
-const path =  require('path');
+const path = require('path');
 
-// This is main configuration object that tells Webpackw what to do. 
 module.exports = {
-    //path to entry paint
-    entry: './src/index.html',
-    //path and filename of the final output
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+    mode: 'development',
+    entry: [
+        "./src/index.html",
+        "./src/index.ts"
+    ],
+    module: {
+        rules: [{
+            // Load .ts & .tsx files via ts-loader
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: '/node_modules/'
+        },
+        // Load .html files via file-loader
+        {
+            test: /\.html/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                }
+            }]
+        }]
     },
-    
-    //default mode is production
-    mode: 'development'
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+    }
 }
